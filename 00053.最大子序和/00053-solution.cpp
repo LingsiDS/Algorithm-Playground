335471cf339c1b792fe2e9d21dd7e22dcc1ddb22
Lingsi <1053781247@qq.com>
2020-10-05 22:13:40 +0800

    add some comments

--- 00053.最大子序和/00053-solution.cpp
+++ 00053.最大子序和/00053-solution.cpp
@@ -18,7 +18,8 @@ public:
 class Solution {
 public:
     struct Node{
-        int sum, s, ls, rs;
+    	//递归过程中需要维护以下信息
+        int sum, s, ls, rs;//sum:本区区间所有元素的和，s:本区间的最大字序和，ls:本区间的最大前缀和，rs:本区间的最大后缀和
     };
 
     Node build(vector<int>& nums, int l, int r) {
@@ -29,10 +30,10 @@ public:
         int mid = l + r >> 1;
         Node ll = build(nums, l, mid), rr = build(nums, mid+1, r);
         Node res;
-        res.sum = ll.sum + rr.sum;
-        res.s = max(max(ll.s, rr.s), ll.rs + rr.ls);
-        res.ls = max(ll.ls, ll.sum + rr.ls);
-        res.rs = max(rr.rs, rr.sum + ll.rs);
+        res.sum = ll.sum + rr.sum;						//本区间所有元素和 = 左右区间所有元素之和
+        res.s = max(max(ll.s, rr.s), ll.rs + rr.ls);	//最大连续子区间 = max(左右子区间最大连续子区间，或者左子区间最大后缀+右子区间最大前缀)
+        res.ls = max(ll.ls, ll.sum + rr.ls);			//最大前缀 = max(左子区间最大前缀，左子区间所有元素和+右子区间最大前缀)
+        res.rs = max(rr.rs, rr.sum + ll.rs);			//最大后缀 = max(右子区间最大后缀，右子区间所有元素和+左子区间最大后缀)
         return res;
     }
 
